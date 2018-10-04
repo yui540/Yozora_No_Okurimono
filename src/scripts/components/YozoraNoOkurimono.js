7c9dbd8988fc58beaf5eaa8be13085c138d07930
yui540 <tabletennis540@gmail.com>
2018-10-04 15:37:05 +0900

    add: chapter4をマークアップ

--- src/scripts/components/YozoraNoOkurimono.js
+++ src/scripts/components/YozoraNoOkurimono.js
@@ -4,6 +4,7 @@ import Home from '../containers/Home'
 import Chapter1 from '../containers/Chapter1'
 import Chapter2 from '../containers/Chapter2'
 import Chapter3 from '../containers/Chapter3'
+import Chapter4 from '../containers/Chapter4'
 
 export default () => (
   <Router>
@@ -12,6 +13,7 @@ export default () => (
       <Route exact path="/chapter/1" component={Chapter1} />
       <Route exact path="/chapter/2" component={Chapter2} />
       <Route exact path="/chapter/3" component={Chapter3} />
+      <Route exact path="/chapter/4" component={Chapter4} />
     </div>
   </Router>
 )
