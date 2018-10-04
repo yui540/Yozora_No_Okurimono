1327ded0977418836b566f384eb3df91e3834b4f
yui540 <tabletennis540@gmail.com>
2018-10-04 15:07:56 +0900

    add: chapter2をマークアップ

--- src/scripts/components/YozoraNoOkurimono.js
+++ src/scripts/components/YozoraNoOkurimono.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { HashRouter as Router, Route } from 'react-router-dom'
 import Home from '../containers/Home'
 import Chapter1 from '../containers/Chapter1'
+import Chapter2 from '../containers/Chapter2'
 
 export default () => (
   <Router>
     <div className="yozora-no-okurimono">
       <Route exact path="/" component={Home} />
       <Route exact path="/chapter/1" component={Chapter1} />
+      <Route exact path="/chapter/2" component={Chapter2} />
     </div>
   </Router>
 )
