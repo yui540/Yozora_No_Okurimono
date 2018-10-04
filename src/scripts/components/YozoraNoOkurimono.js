f96202f4790816a9acdbca30d3830ff304fdbcd2
yui540 <tabletennis540@gmail.com>
2018-10-04 15:17:22 +0900

    add: chapter3をマークアップ

--- src/scripts/components/YozoraNoOkurimono.js
+++ src/scripts/components/YozoraNoOkurimono.js
@@ -3,6 +3,7 @@ import { HashRouter as Router, Route } from 'react-router-dom'
 import Home from '../containers/Home'
 import Chapter1 from '../containers/Chapter1'
 import Chapter2 from '../containers/Chapter2'
+import Chapter3 from '../containers/Chapter3'
 
 export default () => (
   <Router>
@@ -10,6 +11,7 @@ export default () => (
       <Route exact path="/" component={Home} />
       <Route exact path="/chapter/1" component={Chapter1} />
       <Route exact path="/chapter/2" component={Chapter2} />
+      <Route exact path="/chapter/3" component={Chapter3} />
     </div>
   </Router>
 )
